747b20abaef049eb20f2fd58df865a183cdd90b6
jemuelmiao <jemuelmiao@github.com>
2023-09-06 18:12:54 +0800

    MOD:modify type long

--- type_long.go
+++ type_long.go
@@ -22,24 +22,54 @@ package gqlgen_type
 //	w.Write([]byte(fmt.Sprintf("%v", int64(c))))
 //}
 
+//import (
+//	"fmt"
+//	"io"
+//
+//	"github.com/99designs/gqlgen/graphql"
+//)
+//
+//func MarshalMyCustomLongScalar(v int64) graphql.Marshaler {
+//	return graphql.WriterFunc(func(w io.Writer) {
+//		w.Write([]byte(fmt.Sprintf("%v", v)))
+//	})
+//}
+//
+//func UnmarshalMyCustomLongScalar(v interface{}) (int64, error) {
+//	value, ok := v.(int64)
+//	if !ok {
+//		return 0, fmt.Errorf("MyCustomLong must be int64")
+//	}
+//	return value, nil
+//}
+
+
 import (
 	"fmt"
 	"io"
-
-	"github.com/99designs/gqlgen/graphql"
+	"strings"
 )
 
-func MarshalMyCustomLongScalar(v int64) graphql.Marshaler {
-	return graphql.WriterFunc(func(w io.Writer) {
-		w.Write([]byte(fmt.Sprintf("%v", v)))
-	})
+type Banned bool
+
+func (b Banned) MarshalGQL(w io.Writer) {
+	if b {
+		w.Write([]byte("true"))
+	} else {
+		w.Write([]byte("false"))
+	}
 }
 
-func UnmarshalMyCustomLongScalar(v interface{}) (int64, error) {
-	value, ok := v.(int64)
-	if !ok {
-		return 0, fmt.Errorf("MyCustomLong must be int64")
+func (b *Banned) UnmarshalGQL(v interface{}) error {
+	switch v := v.(type) {
+	case string:
+		*b = strings.ToLower(v) == "true"
+		return nil
+	case bool:
+		*b = Banned(v)
+		return nil
+	default:
+		return fmt.Errorf("%T is not a bool", v)
 	}
-	return value, nil
 }
 
