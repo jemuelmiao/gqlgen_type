ec41f454020da00c95c9070ae91800650605fd60
jemuelmiao <jemuelmiao@chery.com>
2023-09-09 22:38:12 +0800

    ADD:add type long contexty

--- type_long_context.go
+++ type_long_context.go
@@ -1,17 +1,19 @@
 package gqlgen_type
 
 import (
+	"context"
 	"fmt"
 	"io"
 )
 
 type LongContext int64
 
-func (l LongContext) MarshalGQL(w io.Writer) {
+func (l LongContext) MarshalGQLContext(ctx context.Context, w io.Writer) error {
 	w.Write([]byte(fmt.Sprintf("%v", l)))
+	return nil
 }
 
-func (l *LongContext) UnmarshalGQL(v interface{}) error {
+func (l *LongContext) UnmarshalGQLContext(ctx context.Context, v interface{}) error {
 	value, ok := v.(int64)
 	if !ok {
 		return fmt.Errorf("LongContext must be int64")
